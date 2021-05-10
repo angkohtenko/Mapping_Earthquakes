dd75d86d3bee0428824105fbc540fc6e809f1838
Angela <angkohtenko@gmail.com>
2021-05-10 16:20:17 -0400

    yellow circle marker on the dark map added

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,15 +5,18 @@ console.log("working");
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Create the tile layer that will be the background of map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 //  Add a marker to the map for Los Angeles, California.
-L.circle([34.0522, -118.2437], {
-    radius: 100
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#Ffff00',
+    fillOpacity: 0.3,
  }).addTo(map);
 
 // Add 'streets' tile layer to the map.
