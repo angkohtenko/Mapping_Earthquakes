abcda34b4ef42934810eb05b686240b9f15a492a
Angela <angkohtenko@gmail.com>
2021-05-10 16:48:54 -0400

    Yellow circle markers on dark map for cities with labels

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -5,7 +5,7 @@ console.log("working");
 let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 
 // Create the tile layer that will be the background of map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -18,7 +18,10 @@ let cityData = cities;
 cityData.forEach(function(city) {
     console.log(city)
     L.circleMarker(city.location,{
-        radius: city.population/100000
+        radius: city.population/100000,
+        color: "orange",
+        fillColor: '#Ffff00',
+        fillOpacity: 0.3,
     })
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map);
