3529288e9e4fbba513765d316d68880138312b8d
Angela <angkohtenko@gmail.com>
2021-05-14 16:36:01 -0400

    Color and size marker based on magntude, pop up marker

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -38,6 +38,25 @@ function getRadius(magnitude) {
   return magnitude * 4;
 }
 
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
 
 // This function returns the style data for each of the earthquakes we plot on the map. We pass the magnitude of the earthquake into a function
 // to calculate the radius.
@@ -45,9 +64,9 @@ function styleInfo(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: "#ffae42",
+    fillColor: getColor(feature.properties.mag),
     color: "#000000",
-    radius: getRadius(),
+    radius: getRadius(feature.properties.mag),
     stroke: true,
     weight: 0.5
   };
@@ -62,9 +81,15 @@ d3.json(earthquake7Days).then(function(data) {
                 console.log(data);
                 return L.circleMarker(latlng);
             },
-    style: styleInfo
+    style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
         }).addTo(map);
 });
+
 
 
 // Pass our map layers into our layers control and add the layers control to the map.
