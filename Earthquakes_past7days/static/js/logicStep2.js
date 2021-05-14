a3fe642273972451bd7a702456cd0aecf98404c3
Angela <angkohtenko@gmail.com>
2021-05-14 16:28:08 -0400

    Orange circle marker based on magnitude

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -28,12 +28,58 @@ let map = L.map('mapid', {
 // Accessing the earthquake GeoJSON URL.
 let earthquake7Days = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
+
+// This function returns the style data for each of the earthquakes we plot on the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(),
+    stroke: true,
+    weight: 0.5
+  };
+}
+
 // Retrieve the earthquake GeoJSON data.
 d3.json(earthquake7Days).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+    // Turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+                console.log(data);
+                return L.circleMarker(latlng);
+            },
+    style: styleInfo
+        }).addTo(map);
 });
 
+// // Grabbing our GeoJSON data.
+// d3.json(torontoData).then(function(data) {
+//   console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   // L.geoJson(data).addTo(map);
+//   L.geoJson(data, {
+//     // Turn each feature into a marker on the map.
+//     onEachFeature: function(feature, layer) {
+//       console.log(layer);
+//       layer.bindPopup("<h2>Airline: " + feature.properties.airline + "</h2> <hr> <h3>Destination: " + feature.properties.dst + "</h3>");
+//     },
+//     style: myStyle
+//   }).addTo(map);
+// });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
